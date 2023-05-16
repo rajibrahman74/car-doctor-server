ce008000f315912c02e75dcebee14d582bb57d09
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-16 23:36:19 +0600

    [ADD]-Create checkout Form and load single service data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,16 +27,30 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const servicesCollection = client.db("carDoctor").collection("services")
+    const servicesCollection = client.db("carDoctor").collection("services");
 
     // get operations
     app.get("/services", async (req, res) => {
-        const cursor = servicesCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    // get opearations for specific data
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
 
+      const options = {
+        // Include only the `title` and `imdb` fields in each returned document
+        projection: { title: 1, price: 1, service_id: 1 },
+      };
+
+      const result = await servicesCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
