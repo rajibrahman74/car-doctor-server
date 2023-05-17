e9010c67cbbd85fa18e1553ef885951784e5d5ad
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-17 17:22:45 +0600

    [ADD]-update booking using Patch

--- index.js
+++ index.js
@@ -69,21 +69,29 @@ async function run() {
       res.send(result);
     });
 
-
     // update bookings data with put operations
-    app.put("/bookings/:id", async(req, res) => {
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
       const updatedBookings = req.body;
-      
-    })
+      console.log(updatedBookings);
 
+      const updateDoc = {
+        $set: {
+          status: updatedBookings.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    });
 
     // delete operations
-    app.delete("/bookings/:id", async(req, res) => {
+    app.delete("/bookings/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await bookingCollection.deleteOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
