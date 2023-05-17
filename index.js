2d8104981f815e55c2469bf82f117899b72a9d45
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-17 16:30:25 +0600

    [ADD]-Create Delete API and Delete a service Booking

--- index.js
+++ index.js
@@ -51,26 +51,38 @@ async function run() {
       res.send(result);
     });
 
-
-
     app.get("/bookings", async (req, res) => {
       console.log(req.query.email);
-      let query = {}
+      let query = {};
       if (req.query?.email) {
-        query = {email: req.query.email}
+        query = { email: req.query.email };
       }
-      const result = await bookingCollection.find(query).toArray()
-      res.send(result)
-    })
-
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // bookings
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       console.log(booking);
-      const result = await bookingCollection.insertOne(booking)
-      res.send(result)
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
 
+
+    // update bookings data with put operations
+    app.put("/bookings/:id", async(req, res) => {
+      const updatedBookings = req.body;
+      
+    })
+
+
+    // delete operations
+    app.delete("/bookings/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
